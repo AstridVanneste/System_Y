4c653d30584d88bfb0d5aa018c8ff1331901f346
FerganDK <fergan.dekock@outlook.com>
2017-10-29 13:50:00 +0100

    Node axel + mij

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -1,6 +1,16 @@
 package Node;
 
 import Network.UDP.Multicast.*;
+import Network.Datagrams.ProtocolHeader;
+import Network.UDP.Unicast.Client;
+import Network.UDP.Unicast.UDPClient;
+import NameServer.ResolverInterface;
+//import NameServer.DiscoveryAgentInterface;
+import NameServer.ShutdownAgentInterface;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 
 public class Node{
@@ -9,11 +19,77 @@ public class Node{
 	private String previousNeighbour;
 	private String nextNeighbour;
 	private Subscriber sub;
+	private String multicastIP = "224.0.0.1";
+	private int multicastPort = 1997;
+	private int id;
+	private Subscriber subscriber;
+	private ResolverInterface resolverInterface;
+	private ShutdownAgentInterface shutdownAgentInterface;
+	//private DiscoveryAgentInterface discoveryAgentInterface;
 
 	public Node(String name){
+		this.resolverInterface=resolverInterface;
+		this.shutdownAgentInterface=shutdownAgentInterface;
+
+		/*System.out.print("Enter the hostName : ");
+		Scanner input = new Scanner(System.in);
+		name = input.nextLine();*/
+
+		try {
+			this.ip = InetAddress.getLocalHost().getHostAddress();
+			this.id = getHash(ip);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
 		this.name = name;
 		//this.ip = giveIp;
 		sub = new Subscriber(ip, 5000);
+
+	}
+
+	public void accessRequest () {
+
+		ProtocolHeader header = new ProtocolHeader();
+		header.setVersion((byte)1);
+		header.setDataLength(0);
+		header.setTransactionID(1);         //is this 'unique' enough?
+		header.setRequestCode(0);
+
+		byte[] serial = header.serialize();
+
+		header.setHeader(serial);
+
+		System.out.println(header.toString());
+
+		UDPClient udpClient = new Client();
+		udpClient.start();
+
+		// Send header with access request
+		udpClient.send(multicastIP, multicastPort, serial );
+
+		// Send name + ip as data
+		byte [] data = new byte[ip.length() + name.length()];
+		System.arraycopy(name, 0 , data,0,name.length());
+		System.arraycopy(ip,0,data,name.length(),ip.length());
+		udpClient.send(multicastIP, multicastPort, data );
+
+	}
+
+
+	public void subscribeOnMulticast () {
+		subscriber = new Subscriber(multicastIP,multicastPort);
+		subscriber.start();
+
+	}
+
+	public void unsubscribeMulticast () {
+		subscriber.stop();
+	}
+
+	public static int getHash(String name) {
+
+		return Math.abs(name.hashCode() % 32768);
 
 	}
 
