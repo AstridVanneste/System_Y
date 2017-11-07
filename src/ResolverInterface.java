a2188317507eb4de3340cae55c0afcf23e41c1f1
FerganDK <fergan.dekock@outlook.com>
2017-11-07 17:05:04 +0100

    update branch

---
+++ src/ResolverInterface.java
@@ -0,0 +1,40 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.security.InvalidParameterException;
+
+/**
+ * Created by Astrid on 22-10-2017.
+ */
+public interface ResolverInterface extends Remote
+{
+	/**
+	 * Translates the ID of a node to its IP address in form of a String
+	 * @param nodeId
+	 * @return
+	 * @throws RemoteException
+	 */
+	public String lookup(int nodeId) throws RemoteException;
+	/**
+	 * Print out the existing TreeMap
+	 * @throws RemoteException
+	 */
+	public void writeMap() throws RemoteException;
+	/**
+	 * Removes entry from TreeMap
+	 * @param nodeId
+	 * @return
+	 * @throws RemoteException
+	 */
+	public void removeFromTree(int nodeId) throws RemoteException;
+
+	/**
+	 * For a given filename it returns the IP address from the owner of the file
+	 * in the form of a String
+	 * @param filename
+	 * @return
+	 * @throws RemoteException, InvalidParameterException
+	 */
+	public String lookup(String filename) throws RemoteException, InvalidParameterException;
+
+	public void addToTree(int ID, String IP) throws RemoteException;   // todo: Remove when RMI testing is complete
+}
