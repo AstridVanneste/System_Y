52a047103f8abca8ba9765b3facd47cc56099a4f
Astrid <astrid.vanneste1@gmail.com>
2017-11-08 11:34:23 +0100

    Shutdown test classes

---
+++ src/Node/ShutdownTest1.java
@@ -0,0 +1,22 @@
+package Node;
+
+import java.util.Scanner;
+
+public class ShutdownTest1
+{
+	public static void main(String args[])
+	{
+		Scanner scanner = new Scanner(System.in);
+
+		Node.getInstance().nameServerBind("10.0.0.1");
+		Node.getInstance().setId((short) 1);
+		Node.getInstance().setNextNeighbour((short) 200);
+		Node.getInstance().setPreviousNeighbour((short) 200);
+
+		System.out.println("Press enter to shutdown the node");
+		scanner.nextLine();
+
+		Node.getInstance().getLifeCycleManager().shutdown();
+
+	}
+}
