e6f82729612c11d3843a481d03728ef027c4a45d
FerganDK <fergan.dekock@outlook.com>
2017-11-13 10:50:27 +0100

    Neighbours in lifecycle

--- src/Node/LifeCycleManager.java
+++ src/Node/LifeCycleManager.java
@@ -1,18 +1,50 @@
 package Node;
 
+import IO.Network.Constants;
+import IO.Network.Datagrams.Datagram;
+import IO.Network.Datagrams.ProtocolHeader;
+import IO.Network.UDP.Multicast.Subscriber;
+import IO.Network.UDP.Unicast.Client;
+import NameServer.NameServer;
+import NameServer.ResolverInterface;
+import NameServer.ShutdownAgentInterface;
+import Util.Serializer;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by Astrid on 07/11/2017.
  */
-public class LifeCycleManager
+public class LifeCycleManager implements NodeInteractionInterface
 {
+	private String name;
+	private String nsIp;
+	private short id;
+	private short previousNeighbour;
+	private short nextNeighbour;
+	private Subscriber subscriber;
+	private ResolverInterface resolverStub;
+	private ShutdownAgentInterface shutdownStub;
+	private int startupTransactionId;
+	private boolean newNode;
+
 	public LifeCycleManager()
 	{
-
+		this.newNode = true;
+		this.resolverStub = null;
+		this.shutdownStub = null;
+		this.id = -1;
+		this.name = "";
+		this.previousNeighbour = -1;
+		this.nextNeighbour = -1;
 	}
 
 	/**
@@ -25,8 +57,8 @@ public class LifeCycleManager
 
 		try
 		{
-			IPprevious = Node.getInstance().getResolverStub().getIP(Node.getInstance().getPreviousNeighbour());
-			IPnext = Node.getInstance().getResolverStub().getIP(Node.getInstance().getNextNeighbour());
+			IPprevious = resolverStub.getIP(previousNeighbour);
+			IPnext = resolverStub.getIP(nextNeighbour);
 		}
 		catch(RemoteException re)
 		{
@@ -38,7 +70,7 @@ public class LifeCycleManager
 		{
 			Registry registryPrev = LocateRegistry.getRegistry(IPprevious);
 			NodeInteractionInterface previousNode = (NodeInteractionInterface) registryPrev.lookup(Node.NODE_INTERACTION_NAME);
-			previousNode.setNextNeighbour(Node.getInstance().getNextNeighbour());
+			previousNode.setNextNeighbour(nextNeighbour);
 		}
 		catch(RemoteException | NotBoundException re)
 		{
@@ -51,7 +83,7 @@ public class LifeCycleManager
 		{
 			Registry registryNext = LocateRegistry.getRegistry(IPnext);
 			NodeInteractionInterface nextNode = (NodeInteractionInterface) registryNext.lookup(Node.NODE_INTERACTION_NAME);
-			nextNode.setPreviousNeighbour(Node.getInstance().getPreviousNeighbour());
+			nextNode.setPreviousNeighbour(previousNeighbour);
 		}
 		catch(RemoteException | NotBoundException re)
 		{
@@ -62,11 +94,318 @@ public class LifeCycleManager
 
 		try
 		{
-			Node.getInstance().getShutdownStub().requestShutdown(Node.getInstance().getId());
+			shutdownStub.requestShutdown(id);
 		}
 		catch(RemoteException re)
 		{
 			re.printStackTrace();
 		}
+	}
+
+	/**
+	 * send a multicast message requezsting to be added to network
+	 */
+	public void accessRequest () {
+		System.out.println("Make accessrequest");
+
+		Client udpClient = new Client();
+		udpClient.start();
+
+		byte version = (byte) 0;
+		short replyCode = (short) 0;
+		short requestCode = ProtocolHeader.REQUEST_DISCOVERY_CODE;
+		Random rand = new Random();
+
+		this.startupTransactionId = rand.nextInt()%127;
+
+		int dataLength = name.length() + 8;
+		ProtocolHeader header = new ProtocolHeader(version, dataLength, startupTransactionId, requestCode, replyCode);
+
+		byte[] data = new byte[name.length() + 8];
+		byte[] nameLengthInByte = Serializer.intToBytes(name.length());
+		byte[] nameInByte = name.getBytes();
+
+		byte[] ipInByte = new byte[4];
+
+		try
+		{
+			ipInByte = Serializer.ipStringToBytes(InetAddress.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
+
+
+		System.arraycopy(nameLengthInByte, 0, data, 0, nameLengthInByte.length);
+		System.arraycopy(nameInByte, 0, data, 4, nameInByte.length);
+		System.arraycopy(ipInByte, 0, data, nameInByte.length + 4, ipInByte.length);
+
+		Datagram datagram = new Datagram(header, data);
+		udpClient.send(Constants.DISCOVERY_MULTICAST_IP, Constants.DISCOVERY_NAMESERVER_PORT, datagram.serialize());
+		udpClient.stop();
+
+		System.out.println("sent accessrequest");
+	}
+
+
+	/**
+	 * Old nodes and new nodes listen on multicast for the acceptance of a new node
+	 * Old nodes will set their new neighbour (new node) with RMI
+	 * New node will update his number of nodes and wait until his neighbours has called his rmi-methods
+	 *
+	 */
+	public void multicastListener()
+	{
+		if(subscriber.hasData())
+		{
+			System.out.println("data received!");
+
+			DatagramPacket packet = subscriber.receivePacket();
+
+			Datagram request = new Datagram(packet.getData());
+			System.out.println(request.getHeader().toString());
+
+			byte[] data = request.getData();
+
+			for(byte b: data)
+			{
+				System.out.print(b + " ");
+			}
+
+			short newNodeID = Serializer.bytesToShort(new byte [] {data[0],data[1]});
+			short numberOfNodes = Serializer.bytesToShort(new byte[] {data[2],data[3]});
+
+			if (!newNode)
+			{
+				if (request.getHeader().getReplyCode() == ProtocolHeader.REPLY_SUCCESSFULLY_ADDED)
+				{
+					changeNeighbours(newNodeID);
+				}
+			}
+			if (newNode)
+			{
+				//check if message contains error message duplicate id from NS
+				if ((request.getHeader().getReplyCode() == ProtocolHeader.REPLY_DUPLICATE_ID) &&
+						(request.getHeader().getTransactionID() == this.startupTransactionId))
+				{
+					askNewName();
+					accessRequest();
+				}
+				
+				//check if message contains error message duplicate id from NS
+				if ((request.getHeader().getReplyCode() == ProtocolHeader.REPLY_DUPLICATE_IP) &&
+						(request.getHeader().getTransactionID() == this.startupTransactionId))
+				{
+					System.out.println("ip already exists!");
+				}
+
+				//check if successfully added to NS
+				if ((request.getHeader().getReplyCode() == (int)ProtocolHeader.REPLY_SUCCESSFULLY_ADDED) &&
+						(request.getHeader().getTransactionID() == this.startupTransactionId))
+				{
+					this.id = newNodeID;
+					this.nsIp = packet.getAddress().getHostAddress();
+					nameServerBind(nsIp);
+					//nameserver sends the amount of nodes in the tree
+					if(numberOfNodes == 0)
+					{
+						this.nextNeighbour = this.id;
+						this.previousNeighbour = this.id;
+					}
+					newNode = false;
+					System.out.println("toegevoegd");
+				}
+			}
+		}
+	}
+
+	public void nameServerBind(String nsIp){
+		if(System.getSecurityManager()==null)
+		{
+			System.setSecurityManager(new SecurityManager());
+		}
+		Registry reg = null;
+		try
+		{
+			reg = LocateRegistry.getRegistry(nsIp);
+			this.resolverStub = (ResolverInterface) reg.lookup(NameServer.RESOLVER_NAME);
+			this.shutdownStub = (ShutdownAgentInterface) reg.lookup((NameServer.SHUTDOWN_AGENT_NAME));
+		} catch (RemoteException e)
+		{
+			e.printStackTrace();
+		} catch (NotBoundException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Check which neighbour the new incoming neighbour becomes
+	 */
+	private synchronized void changeNeighbours(short newID)
+	{
+		// You'r the first node so edit both neighbours of the new node
+		if(this.id == this.nextNeighbour && this.id == this.previousNeighbour)
+		{
+			this.nextNeighbour = newID;
+			this.previousNeighbour = newID;
+			Registry reg = null;
+			try
+			{
+				NodeInteractionInterface neighbourInterface = null;
+				/*try
+				{
+					//neighbourInterface = (NodeInteractionInterface) Naming.lookup("//"+ resolverStub.getIP(newID) + "/" + Node.NODE_INTERACTION_NAME);
+
+				} catch (MalformedURLException e)
+				{
+					e.printStackTrace();
+				}*/
+				reg = LocateRegistry.getRegistry(resolverStub.getIP(newID));
+				neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
+				neighbourInterface.setNextNeighbour(id);
+				neighbourInterface.setPreviousNeighbour(id);
+			} catch (RemoteException e)
+			{
+				e.printStackTrace();
+			}catch (NotBoundException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		// The node will change the previous and next id of the new node in following cases:
+		//you are the previous of the new node and need to change the neighbours of the new node and your old next neighbour
+		if (
+				(		(newID > this.id && newID > this.nextNeighbour && this.nextNeighbour < this.id) ||
+						(newID > this.id && newID < this.nextNeighbour && this.nextNeighbour > this.id) ||
+						(newID < this.id && newID < this.nextNeighbour && this.nextNeighbour < this.id)
+
+				))
+		{
+			Registry reg = null;
+			try
+			{
+				NodeInteractionInterface neighbourInterface = null;
+				reg = LocateRegistry.getRegistry(this.resolverStub.getIP(newID));
+				neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
+				neighbourInterface.setNextNeighbour(this.nextNeighbour);
+				neighbourInterface.setPreviousNeighbour(this.id);
+
+				reg = LocateRegistry.getRegistry(this.resolverStub.getIP(this.nextNeighbour));
+				neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
+				neighbourInterface.setPreviousNeighbour(newID);
+				this.nextNeighbour = newID;
+
+				//reg = LocateRegistry.getRegistry(resolverStub.getIP(newID));
+				//NodeInteractionInterface neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
+			} catch (RemoteException e)
+			{
+				e.printStackTrace();
+			} catch (NotBoundException e)
+			{
+				e.printStackTrace();
+			}
+
+		}
+	/*
+		// The node will change the previous and next id of the new node in following cases:
+		// nothing needs to be done
+		if (
+				(		(newID < this.id && newID > this.previousNeighbour && this.previousNeighbour < this.id)||
+						(newID < this.id && newID < this.previousNeighbour && this.previousNeighbour > this.id) ||
+						(newID > this.id && newID > this.previousNeighbour && this.previousNeighbour > this.id)
+				))
+		{
+			//this.previousNeighbour = newID;
+		}
+		*/
+	}
+
+	/**
+	 * Subscribe this node on the multicast-address 224.0.0.1
+	 */
+	public void subscribeOnMulticast ()
+	{
+		this.subscriber = new Subscriber(Constants.DISCOVERY_MULTICAST_IP,Constants.DISCOVERY_CLIENT_PORT);
+		this.subscriber.start();
+	}
+
+	/**
+	 * Unsubscribe this node on the multicast-address 224.0.0.1
+	 */
+	public void unsubscribeMulticast ()
+	{
+		this.subscriber.stop();
+	}
+
+
+	/**
+	 * at startup, set the neighbours as your own
+	 */
+	private void setNeighbours()
+	{
+		this.previousNeighbour = this.id;
+		this.nextNeighbour = this.id;
+	}
+
+	private void askNewName ()
+	{
+		System.out.println("Please enter a new name: ");
+		Scanner scanner = new Scanner(System.in);
+		this.name = scanner.nextLine();
+		this.id = NameServer.getHash(name);
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public short getPreviousNeighbour() throws RemoteException
+	{
+		return this.previousNeighbour;
+	}
+
+	public void setPreviousNeighbour(short previousNeighbour) throws RemoteException
+	{
+		this.previousNeighbour = previousNeighbour;
+	}
+
+	public short getNextNeighbour() throws RemoteException
+	{
+		return this.nextNeighbour;
+	}
+
+	public void setNextNeighbour(short nextNeighbour) throws RemoteException
+	{
+		this.nextNeighbour = nextNeighbour;
+	}
+
+	public ResolverInterface getResolverStub()
+	{
+		return this.resolverStub;
+	}
+
+	public ShutdownAgentInterface getShutdownStub()
+	{
+		return this.shutdownStub;
+	}
+
+	public short getId()
+	{
+		return this.id;
+	}
+
+	public void setId(short id)
+	{
+		this.id = id;
 	}
 }
--- src/Node/Node.java
+++ src/Node/Node.java
@@ -25,39 +25,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class Node implements Runnable, NodeInteractionInterface
+public class Node implements Runnable
 {
 	public static final String NODE_INTERACTION_NAME = "NODE_INTERACTION";
 
 	private LifeCycleManager lifeCycleManager;
 	private FailureAgent failureAgent;
 
-	private String name;
-	private String nsIp;
-	private short id;
-	private short previousNeighbour;
-	private short nextNeighbour;
-	private Subscriber subscriber;
-	private ResolverInterface resolverStub;
-	private ShutdownAgentInterface shutdownStub;
-	private short numberOfNodes;
-	private int startupTransactionId;
-	private boolean newNode;
+
 
 	private static Node instance;
 
 
 	public Node()
 	{
-		this.newNode = true;
-		this.resolverStub = null;
-		this.shutdownStub = null;
-		this.id = -1;
-		this.name = "";
 		this.lifeCycleManager = new LifeCycleManager();
 		this.failureAgent = new FailureAgent();
-		this.previousNeighbour = -1;
-		this.nextNeighbour = -1;
+
 	}
 
 	public static Node getInstance()
@@ -74,7 +58,7 @@ public class Node implements Runnable, NodeInteractionInterface
 	{
 		System.out.println("give me a name");
 		Scanner scanner = new Scanner(System.in);
-		this.name = scanner.nextLine();
+		lifeCycleManager.setName(scanner.nextLine());
 		if(System.getSecurityManager()==null)
 		{
 			System.setSecurityManager(new SecurityManager());
@@ -82,9 +66,9 @@ public class Node implements Runnable, NodeInteractionInterface
 
 		try
 		{
-			subscribeOnMulticast();
+			lifeCycleManager.subscribeOnMulticast();
 
-			NodeInteractionInterface stub = (NodeInteractionInterface) UnicastRemoteObject.exportObject(this,0);
+			NodeInteractionInterface stub = (NodeInteractionInterface) UnicastRemoteObject.exportObject(lifeCycleManager,0);
 			Registry registry = LocateRegistry.createRegistry(1099);
 			registry.bind(Node.NODE_INTERACTION_NAME, stub);
 
@@ -98,7 +82,7 @@ public class Node implements Runnable, NodeInteractionInterface
 			e.printStackTrace();
 		}
 
-		this.accessRequest();
+		lifeCycleManager.accessRequest();
 
 		Thread thread = new Thread(this);
 		thread.start();
@@ -108,234 +92,13 @@ public class Node implements Runnable, NodeInteractionInterface
 	 * Multicast for NS
 	 * NS will process this message and has to confirm that the node may access the network
 	 */
-	public void accessRequest () {
-		System.out.println("Make accessrequest");
 
-		Client udpClient = new Client();
-		udpClient.start();
-
-		byte version = (byte) 0;
-		short replyCode = (short) 0;
-		short requestCode = ProtocolHeader.REQUEST_DISCOVERY_CODE;
-		Random rand = new Random();
-
-		this.startupTransactionId = rand.nextInt()%127;
-
-		int dataLength = name.length() + 8;
-		ProtocolHeader header = new ProtocolHeader(version, dataLength, startupTransactionId, requestCode, replyCode);
-
-		byte[] data = new byte[name.length() + 8];
-		byte[] nameLengthInByte = Serializer.intToBytes(name.length());
-		byte[] nameInByte = name.getBytes();
-
-		byte[] ipInByte = new byte[4];
-
-		try
-		{
-			ipInByte = Serializer.ipStringToBytes(InetAddress.getLocalHost().getHostAddress());
-		} catch (UnknownHostException e)
-		{
-			e.printStackTrace();
-		}
-
-
-		System.arraycopy(nameLengthInByte, 0, data, 0, nameLengthInByte.length);
-		System.arraycopy(nameInByte, 0, data, 4, nameInByte.length);
-		System.arraycopy(ipInByte, 0, data, nameInByte.length + 4, ipInByte.length);
-
-		Datagram datagram = new Datagram(header, data);
-		udpClient.send(Constants.DISCOVERY_MULTICAST_IP, Constants.DISCOVERY_NAMESERVER_PORT, datagram.serialize());
-		udpClient.stop();
-
-		System.out.println("sent accessrequest");
-	}
-
-
-	/**
-	 * Old nodes and new nodes listen on multicast for the acceptance of a new node
-	 * Old nodes will set their new neighbour (new node) with RMI
-	 * New node will update his number of nodes and wait until his neighbours has called his rmi-methods
-	 *
-	 */
-	public void multicastListener()
-	{
-		if(subscriber.hasData())
-		{
-			System.out.println("data received!");
-
-			DatagramPacket packet = subscriber.receivePacket();
-
-			Datagram request = new Datagram(packet.getData());
-			System.out.println(request.getHeader().toString());
-
-			byte[] data = request.getData();
-
-			for(byte b: data)
-			{
-				System.out.print(b + " ");
-			}
-
-			short newNodeID = Serializer.bytesToShort(new byte [] {data[0],data[1]});
-			numberOfNodes = Serializer.bytesToShort(new byte[] {data[2],data[3]});
-
-			System.out.println("new node id: " + newNodeID);
-
-			if (!newNode)
-			{
-				if (request.getHeader().getReplyCode() == ProtocolHeader.REPLY_SUCCESSFULLY_ADDED)
-				{
-					changeNeighbours(newNodeID);
-					this.numberOfNodes = Serializer.bytesToShort(new byte[]{data[2],data[3]});
-				}
-			}
-			if (newNode)
-			{
-				//check if message contains error message from NS
-				if ((request.getHeader().getReplyCode() == ProtocolHeader.REPLY_DUPLICATE_ID) &&
-						(request.getHeader().getTransactionID() == this.startupTransactionId))
-				{
-					askNewName();
-					accessRequest();
-					System.out.println("niet toegevoegd");
-				}
-
-				//check if successfully added to NS
-				if ((request.getHeader().getReplyCode() == (int)ProtocolHeader.REPLY_SUCCESSFULLY_ADDED) &&
-						(request.getHeader().getTransactionID() == this.startupTransactionId))
-				{
-					//nameserver sends the amount of nodes in the tree
-					setNumberOfNodes(numberOfNodes);
-					this.id = newNodeID;
-					System.out.println("own ID: " + newNodeID);
-					this.nsIp = packet.getAddress().getHostAddress();
-					System.out.println("nameserver ip : " + this.nsIp);
-					nameServerBind(nsIp);
-					if(numberOfNodes == 0)
-					{
-						this.nextNeighbour = this.id;
-						this.previousNeighbour = this.id;
-					}
-					newNode = false;
-					System.out.println("toegevoegd");
-				}
-			}
-		}
-	}
-
-	public void nameServerBind(String nsIp){
-		if(System.getSecurityManager()==null)
-		{
-			System.setSecurityManager(new SecurityManager());
-		}
-		Registry reg = null;
-		try
-		{
-			reg = LocateRegistry.getRegistry(nsIp);
-			this.resolverStub = (ResolverInterface) reg.lookup(NameServer.RESOLVER_NAME);
-			this.shutdownStub = (ShutdownAgentInterface) reg.lookup((NameServer.SHUTDOWN_AGENT_NAME));
-		} catch (RemoteException e)
-		{
-			e.printStackTrace();
-		} catch (NotBoundException e)
-		{
-			e.printStackTrace();
-		}
-
-	}
-
-	/**
-	 * Check which neighbour the new incoming neighbour becomes
-	 */
-	private synchronized void changeNeighbours(short newID)
-	{
-		// You'r the first node so edit both neighbours of the new node
-		if(this.id == this.nextNeighbour && this.id == this.previousNeighbour)
-		{
-			this.nextNeighbour = newID;
-			this.previousNeighbour = newID;
-			System.out.println("Next for old node " + this.nextNeighbour);
-			System.out.println("previous for old node " + this.previousNeighbour);
-			Registry reg = null;
-			try
-			{
-				NodeInteractionInterface neighbourInterface = null;
-				/*try
-				{
-					//neighbourInterface = (NodeInteractionInterface) Naming.lookup("//"+ resolverStub.getIP(newID) + "/" + Node.NODE_INTERACTION_NAME);
-
-				} catch (MalformedURLException e)
-				{
-					e.printStackTrace();
-				}*/
-				reg = LocateRegistry.getRegistry(resolverStub.getIP(newID));
-				System.out.println(resolverStub.getIP(newID));
-				neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
-				neighbourInterface.setNextNeighbour(id);
-				neighbourInterface.setPreviousNeighbour(id);
-			} catch (RemoteException e)
-			{
-				e.printStackTrace();
-			}catch (NotBoundException e)
-			{
-				e.printStackTrace();
-			}
-		}
-
-		// The node will change the previous and next id of the new node in following cases:
-		//you are the previous of the new node and need to change the neighbours of the new node and your old next neighbour
-		if (
-				(		(newID > this.id && newID > this.nextNeighbour && this.nextNeighbour < this.id) ||
-						(newID > this.id && newID < this.nextNeighbour && this.nextNeighbour > this.id) ||
-						(newID < this.id && newID < this.nextNeighbour && this.nextNeighbour < this.id)
-
-				))
-		{
-			Registry reg = null;
-			try
-			{
-				NodeInteractionInterface neighbourInterface = null;
-				reg = LocateRegistry.getRegistry(this.resolverStub.getIP(newID));
-				neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
-				neighbourInterface.setNextNeighbour(this.nextNeighbour);
-				neighbourInterface.setPreviousNeighbour(this.id);
-
-				reg = LocateRegistry.getRegistry(this.resolverStub.getIP(this.nextNeighbour));
-				neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
-				neighbourInterface.setPreviousNeighbour(newID);
-				this.nextNeighbour = newID;
-
-				//reg = LocateRegistry.getRegistry(resolverStub.getIP(newID));
-				//NodeInteractionInterface neighbourInterface = (NodeInteractionInterface) reg.lookup(Node.NODE_INTERACTION_NAME);
-			} catch (RemoteException e)
-			{
-				e.printStackTrace();
-			} catch (NotBoundException e)
-			{
-				e.printStackTrace();
-			}
-
-
-			System.out.println("Next for old node " + this.nextNeighbour);
-		}
-	/*
-		// The node will change the previous and next id of the new node in following cases:
-		// nothing needs to be done
-		if (
-				(		(newID < this.id && newID > this.previousNeighbour && this.previousNeighbour < this.id)||
-						(newID < this.id && newID < this.previousNeighbour && this.previousNeighbour > this.id) ||
-						(newID > this.id && newID > this.previousNeighbour && this.previousNeighbour > this.id)
-				))
-		{
-			//this.previousNeighbour = newID;
-		}
-		*/
-	}
 
 	public void run()
 	{
 		while(true)
 		{
-			multicastListener();
+			lifeCycleManager.multicastListener();
 			try
 			{
 				Thread.sleep(1);
@@ -346,62 +109,6 @@ public class Node implements Runnable, NodeInteractionInterface
 		}
 	}
 
-	/**
-	 * Subscribe this node on the multicast-address 224.0.0.1
-	 */
-	public void subscribeOnMulticast ()
-	{
-		this.subscriber = new Subscriber(Constants.DISCOVERY_MULTICAST_IP,Constants.DISCOVERY_CLIENT_PORT);
-		this.subscriber.start();
-	}
-
-	/**
-	 * Unsubscribe this node on the multicast-address 224.0.0.1
-	 */
-	public void unsubscribeMulticast ()
-	{
-		this.subscriber.stop();
-	}
-
-	/**
-	 * If this node has no neighbours
-	 */
-	private void setNeighbours()
-	{
-		this.previousNeighbour = this.id;
-		this.nextNeighbour = this.id;
-	}
-
-	/**
-	 * If this node has 1 neighbours
-	 */
-	private void setNeighbours(short neighbourId)
-	{
-		this.previousNeighbour = neighbourId;
-		this.nextNeighbour = neighbourId;
-	}
-
-	/**
-	 * If this node has 2 neighbours
-	 */
-	private void setNeighbours(short previousNeighbour, short nextNeighbour)
-	{
-		this.previousNeighbour = previousNeighbour;
-		this.nextNeighbour = nextNeighbour;
-	}
-
-	private void askNewName ()
-	{
-		System.out.println("Please enter a new name: ");
-		Scanner scanner = new Scanner(System.in);
-		this.name = scanner.nextLine();
-		this.id = NameServer.getHash(name);
-	}
-
-	public String getName()
-	{
-		return this.name;
-	}
 
 	public LifeCycleManager getLifeCycleManager()
 	{
@@ -413,63 +120,5 @@ public class Node implements Runnable, NodeInteractionInterface
 		return this.failureAgent;
 	}
 
-	//MOET VEEL WEG
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-
-	public short getPreviousNeighbour() throws RemoteException
-	{
-		return this.previousNeighbour;
-	}
-
-	public void setPreviousNeighbour(short previousNeighbour) throws RemoteException
-	{
-		this.previousNeighbour = previousNeighbour;
-		System.out.println("Previous for new Node : " + previousNeighbour);
-	}
-
-	public short getNextNeighbour() throws RemoteException
-	{
-		return this.nextNeighbour;
-
-	}
-
-	public void setNextNeighbour(short nextNeighbour) throws RemoteException
-	{
-		this.nextNeighbour = nextNeighbour;
-		System.out.println("Next for new Node : " + nextNeighbour);
-	}
-
-	public ResolverInterface getResolverStub()
-	{
-		return this.resolverStub;
-	}
-
-	public ShutdownAgentInterface getShutdownStub()
-	{
-		return this.shutdownStub;
-	}
-
-	public short getId()
-	{
-		return this.id;
-	}
-
-	public void setId(short id)
-	{
-		this.id = id;
-	}
-
-	public short getNumberOfNodes()
-	{
-		return numberOfNodes;
-	}
-
-	public void setNumberOfNodes(short numberOfNodes)
-	{
-		this.numberOfNodes = numberOfNodes;
-	}
 
 }
