6f6c1e7847f44fa4e7cf8f4dcd6f17374f2bcb8e
axelclaeijs <axelclaeijs@gmail.com>
2017-11-20 22:31:35 +0100

    update UpdateAgent

--- src/Node/UpdateAgent.java
+++ src/Node/UpdateAgent.java
@@ -2,66 +2,94 @@ package Node;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class UpdateAgent implements Runnable
 {
 	private WatchService service;
-	private Path path;
-	private Map keyMap;
-	private WatchKey watchkey;
-	public UpdateAgent(){
+	private final Path LOCAL_DIR;
+	private WatchKey watchKey;
 
+	public UpdateAgent()
+	{
+		this.LOCAL_DIR = Paths.get("Local/");
 	}
 
-	public void start(){
-		try{
-			service = FileSystems.getDefault().newWatchService();
-			keyMap= new HashMap<WatchKey, Path>();
+	public void start()
+	{
+		try
+		{
+			this.service = FileSystems.getDefault().newWatchService();
 
-			//directory of files
-			path = Paths.get("Files");
+			LOCAL_DIR.register(service, StandardWatchEventKinds.ENTRY_CREATE);
 
 			Thread thread = new Thread(this);
 			thread.start();
 
-			//specify which entries should be watched. in this case only the creation of  a file will be watched.
-			keyMap.put(path.register(service,StandardWatchEventKinds.ENTRY_CREATE),path);
-		} catch (IOException e)
+		}
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
 
 	}
 
-	public void run(){
+	public void run()
+	{
 		watcher();
 	}
 
-	public void watcher(){
-		try
+	public void watcher()
+	{
+		for (;;)
 		{
-			watchkey = service.take();
-			Path eventDir = (Path)keyMap.get(watchkey);
-			 for(WatchEvent<?> event : watchkey.pollEvents()){
+			// wait for key to be signalled
+			try
+			{
+				watchKey = service.take();
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+				return;
+			}
+
+			Path eventDir = (Path) watchKey.watchable();
+			if (eventDir == null)
+			{
+				System.err.println("WatchKey not recognized!");
+				continue;
+			}
+
+			for (WatchEvent<?> event : watchKey.pollEvents())
+			{
 				WatchEvent.Kind<?> kind = event.kind();
-				Path eventPath = (Path)event.context();
-			 	System.out.println(eventDir +  " :" + kind + " : " + eventPath);
-			 }
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
+				Path eventPath = (Path) event.context();
+				System.out.println(eventDir + " :" + kind + " : " + eventPath);
+
+				// this check is necessary just in case
+				if (kind == StandardWatchEventKinds.OVERFLOW) {
+					continue;
+				}
+
+				if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+					System.out.println("File created");
+					//handle...
+					continue;
+				}
+
+			}
+
+			watchKey.reset();
+
 		}
-
-
 	}
 
 	public void stop(){
 		try
 		{
 			service.close();
-		} catch (IOException e)
+		}
+		catch (IOException e)
 		{
 			e.printStackTrace();
 		}
